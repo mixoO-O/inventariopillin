30ec5d5f34e7817ca3124f5d1233a48d387edd06
Felipeplz <36702371+Felipeplz@users.noreply.github.com>
2018-03-24 20:26:06 -0300

    Add files via upload
    Navbar 1.01

--- views/includes/menu.php
+++ views/includes/menu.php
@@ -27,14 +27,10 @@
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           <?=$_SESSION['user']?>
         </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
           <a class="dropdown-item" href="../controllers/salir.php">Cerrar Sesión</a>
         </div>
       </li>
     </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-secondary my-2 my-sm-0" type="submit">Search</button>
-    </form>
   </div>
 </nav>
