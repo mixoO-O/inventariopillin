5a6df98392b036b31ce48f28fa853e4f6e70e15d
Peter <peterdotpy@gmail.com>
2018-03-24 02:16:44 -0300

    Add files via upload

---
+++ views/home.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Inventario Pillin</title>
+
+  <link rel="stylesheet" href="../views/public/css/bootstrap.min.css">
+</head>
+<body>
+  <header>
+    <?php require_once('../views/includes/menu.php');
+
+    echo "<pre>";
+    print_r($menu);
+    echo "</pre>";
+
+    ?>
+  </header>
+
+  <section>
+
+  </section>
+
+  <footer>
+
+  </footer>
+
+  <script src="../views/public/js/jquery-3.2.1.slim.min.js"></script>
+  <script src="../views/public/js/bootstrap.min.js"></script>
+</body>
+</html>
