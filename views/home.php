fde0bebdf0dc75d8ab0866fec20d15b81106756a
Felipeplz <36702371+Felipeplz@users.noreply.github.com>
2018-03-24 21:17:23 -0300

    Add files via upload

--- views/home.php
+++ views/home.php
@@ -9,23 +9,18 @@
   <link rel="stylesheet" href="../views/public/css/bootstrap.min.css">
 </head>
 <body>
-  <header>
-    <?php require_once('../views/includes/menu.php');
-
-    echo "<pre>";
-    print_r($menu);
-    echo "</pre>";
-
-    ?>
-  </header>
-
-  <section>
-
-  </section>
-
-  <footer>
-
-  </footer>
+    <header>
+      <?php require_once('../views/includes/menu.php'); ?>
+    </header>
+    <div class="cover-container d-flex h-100 p-3 mx-auto flex-column text-center">
+      <main role="main" class="inner cover">
+        <h1 class="cover-heading">¡Bienvenido!</h1>
+        <p class="lead">Para subir tus archivos, guíate con la barra de navegación que se encuentra arriba del todo o en el siguiente botón.</p>
+        <p class="lead">
+          <a href="../controllers/subir_archivos.php" class="btn btn-lg btn-secondary">Llévame</a>
+        </p>
+      </main>
+    </div>
 
   <script src="../views/public/js/jquery-3.2.1.slim.min.js"></script>
   <script src="../views/public/js/bootstrap.min.js"></script>
