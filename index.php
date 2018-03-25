5bd7f0eb2e7f4c895bbea5d7e4f74dbb435a25d9
Peter <peterdotpy@gmail.com>
2018-03-25 03:52:37 -0300

    Actualización

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>Login V1</title>
+	<title>LOGIN PILLIN</title>
 	<meta charset="UTF-8">
 	<meta name="viewsport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" type="text/css" href="views/public/font-awesome-4.7.0/css/font-awesome.min.css">
