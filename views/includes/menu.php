06ab58c1cb64f399fe2c301b208f587fdc4dcf2a
Felipeplz <36702371+Felipeplz@users.noreply.github.com>
2018-03-24 21:24:47 -0300

    Add files via upload

--- views/includes/menu.php
+++ views/includes/menu.php
@@ -1,5 +1,5 @@
 <nav class="navbar header-navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="../controllers/home.php">Pillin</a>
+  <a class="navbar-brand" href="../controllers/home.php">PILLIN</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -14,14 +14,14 @@
 
       <?php foreach ($menu as $value) { ?>
         <li class="nav-item">
-          <a class="nav-link" href="<?=$value['link']?>"><?=$value['nombre']?></a>
+          <a class="nav-link" href="<?=$value['link']?>"><?=strtoupper($value['nombre'])?></a>
         </li>
       <?php } ?>
     </ul>
     <ul class="navbar-nav ml-auto">
       <li class="nav-item dropdown ml-auto">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          <?=$_SESSION['user']?>
+          <?=strtoupper($_SESSION['user'])?>
         </a>
         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
           <a class="dropdown-item" href="../controllers/salir.php">Cerrar Sesión</a>
